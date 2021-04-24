cc2cfbaa04ea09b14da2586aa3ff84af2633f428
Heinrich Venter <heini141@outlook.com>
2021-04-24 14:30:44 +0200

    updated code

--- src/app/components/wedding-date-countdown/wedding-date-countdown.component.ts
+++ src/app/components/wedding-date-countdown/wedding-date-countdown.component.ts
@@ -8,28 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class WeddingDateCountdownComponent implements OnInit {
 
   constructor() { }
-  countdown = "";
+  countdown = '';
   ngOnInit(): void {
     window.setInterval(() => this.getCountdownValue(), 1000);
   }
 
-  getCountdownValue() {
-    var countDownDate = new Date("Nov 18, 2021 16:30:00").getTime();
+  getCountdownValue(): void {
+    const countDownDate = new Date('Nov 18, 2021 16:30:00').getTime();
 
     // Get today's date and time
-    let now = new Date().getTime();
+    const now = new Date().getTime();
 
     // Find the distance between now and the count down date
-    let distance = countDownDate - now;
+    const distance = countDownDate - now;
 
     // Time calculations for days, hours, minutes and seconds
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
-    this.countdown = days + ' Days';
+    this.countdown = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
 
     // If the count down is finished, write some text
     if (distance < 0) {
--- src/app/views/photos/photos.component.ts
+++ src/app/views/photos/photos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { gallery } from '../../static/gallery';
 
 @Component({
   selector: 'app-photos',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotosComponent implements OnInit {
 
+  photos = gallery.photos;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
