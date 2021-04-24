e46e5532584c82c93df307129f4222f2b16de0c7
Heinrich Venter <heini141@outlook.com>
2021-04-24 12:55:53 +0200

    added photos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './views/admin/admin.component';
 import { HomeComponent } from './views/home/home.component';
+import { PhotosComponent } from './views/photos/photos.component';
 import { RsvpRedirectComponent } from './views/rsvp/rsvp-redirect/rsvp-redirect.component';
 import { RsvpComponent } from './views/rsvp/rsvp.component';
 import { SuggestASongComponent } from './views/suggest-a-song/suggest-a-song.component';
@@ -33,6 +34,10 @@ const routes: Routes = [
   {
     path: 'venue-info',
     component: VenueInfoComponent
+  },
+  {
+    path: 'gallery',
+    component: PhotosComponent
   },
   {
     path: '',
